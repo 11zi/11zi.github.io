57fe8db5ca3cb0043d7e84af2037e237aa22215a
swj <971210502@qq.com>
2019-05-02 08:28:45 +0800

    更新评论列表以及不能评论的评论区

--- src/javascript/study.js
+++ src/javascript/study.js
@@ -1,26 +1,39 @@
 (function () {
   axios.get('https://api.github.com/repos/11zi/11zi.github.io/labels').then(function (response) {
     var labels = response.data
-    var labelsElement = document.getElementById('labels')
+    var labelElement = document.getElementById('labels')
     for (var i = labels.length - 1; i >= 0; i--) {
       var append = '<div class="mdui-chip mdui-m-r-1" style="background-color:#' + labels[i].color + '"><span class="mdui-chip-title">' + labels[i].name + '</span></div>'
-      labelsElement.innerHTML += append
+      labelElement.innerHTML += append
     }
   })
-})();
+})();// 标签列表
 (function () {
   axios.get('https://api.github.com/repos/11zi/11zi.github.io/issues').then(function (response) {
     var articles = response.data
+    var articleElement = document.getElementById('articles')
     for (var i = 0; i < articles.length - 1; i++) {
-      var articleElement = document.getElementById('articles')
       var append = '<div class="mdui-col-xs-11 mdui-typo"><blockquote><article>'
       append += '<small class="mdui-float-right"><i class="mdui-icon material-icons">&#xe8df;</i>' + (articles[i].updated_at).slice(0, 10) + '</small>'
       append += '<h1>' + articles[i].title + "</h1>"
       for (var j = articles[i].labels.length - 1; j >= 0; j--) {
-        append += '<div class="mdui-chip mdui-m-r-1" style="background-color:#' + articles[i].labels[j].color + '"><span class="mdui-chip-title">' + articles[i].labels[j].name + '</span></div>'
+        append += '<div class="mdui-chip mdui-m-r-1" style="color:white;background-color:#' + articles[i].labels[j].color + '"><span class="mdui-chip-title">' + articles[i].labels[j].name + '</span></div>'
       }
       append += '</article></blockquote></div>'
       articleElement.innerHTML += append
     }
   })
-})();
+})();// 文章列表
+(function () {
+  axios.get('https://api.github.com/repos/11zi/11zi.github.io/issues/1/comments').then(function (response) {
+    var comments = response.data
+    var commentElement = document.getElementById('comments')
+    for (var i = comments.length - 1; i >= 0; i--) {
+      var append = '<div class="mdui-row mdui-ripple"><label class="mdui-list-item"><div class="mdui-list-item-avatar">'
+      append += '<img src="' + comments[i].user.avatar_url + '" /></div>'
+      append += '<div class="mdui-list-item-content">' + comments[i].user.login + '</div>'
+      append += '</label><p class="mdui-m-l-2 mdui-m-r-2 mdui-m-t-1 mdui-m-b-1">' + comments[i].body + '</p></div>'
+      commentElement.innerHTML += append
+    }
+  })
+}());// 评论列表
