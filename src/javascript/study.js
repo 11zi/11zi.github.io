8b2fc9d30912f90c74e6f7db8582f00c23ddca4e
swj <971210502@qq.com>
2019-05-21 08:31:50 +0800

    okay no pb!

--- src/javascript/study.js
+++ src/javascript/study.js
@@ -9,22 +9,6 @@
   })
 })();// 标签列表
 (function () {
-  axios.get('https://api.github.com/repos/11zi/11zi.github.io/issues').then(function (response) {
-    var articles = response.data
-    var articleElement = document.getElementById('articles')
-    for (var i = 0; i < articles.length - 1; i++) {
-      var append = '<div class="mdui-col-xs-11 mdui-typo"><blockquote><article>'
-      append += '<small class="mdui-float-right"><i class="mdui-icon material-icons">&#xe8df;</i>' + (articles[i].updated_at).slice(0, 10) + '</small>'
-      append += '<h1><a href="javascript:void(0);" onclick="getArticle('+(i)+')">' + articles[i].title + "</a></h1>"
-      for (var j = articles[i].labels.length - 1; j >= 0; j--) {
-        append += '<div class="mdui-chip mdui-m-r-1" style="color:white;background-color:#' + articles[i].labels[j].color + '"><span class="mdui-chip-title">' + articles[i].labels[j].name + '</span></div>'
-      }
-      append += '</article></blockquote></div>'
-      articleElement.innerHTML += append
-    }
-  })
-})();// 文章列表
-(function () {
   axios.get('https://api.github.com/repos/11zi/11zi.github.io/issues/1/comments').then(function (response) {
     var comments = response.data
     var commentElement = document.getElementById('comments')
@@ -37,14 +21,35 @@
     }
   })
 }());// 评论列表
+function listArticles() {
+  axios.get('https://api.github.com/repos/11zi/11zi.github.io/issues').then(function (response) {
+    var articles = response.data
+    var articleElement = document.getElementById('articles')
+    var htmlList = ''
+    for (var i = 0; i < articles.length - 1; i++) {
+      var append = '<div class="mdui-col-xs-11 mdui-typo"><blockquote><article>'
+      append += '<small class="mdui-float-right"><i class="mdui-icon material-icons">&#xe8df;</i>' + (articles[i].updated_at).slice(0, 10) + '</small>'
+      append += '<h1><a href="javascript:void(0);" onclick="getArticle(' + (articles.length - i) + ')">' + articles[i].title + "</a></h1>"
+      for (var j = articles[i].labels.length - 1; j >= 0; j--) {
+        append += '<div class="mdui-chip mdui-m-r-1" style="color:white;background-color:#' + articles[i].labels[j].color + '"><span class="mdui-chip-title">' + articles[i].labels[j].name + '</span></div>'
+      }
+      append += '</article></blockquote></div>'
+      htmlList += append
+    }
+    articleElement.innerHTML = htmlList
+  })
+};// 展示文章列表
 function getArticle(issueID) {
-  axios.get('https://api.github.com/repos/11zi/11zi.github.io/issues/'+issueID).then(function (response) {
+  axios.get('https://api.github.com/repos/11zi/11zi.github.io/issues/' + issueID).then(function (response) {
     console.log("Okay no pb.")
     var converter = new showdown.Converter()
     var article = response.data.body
-    var html = converter.makeHtml(article)
+    var html = '<div class="mdui-col-xs-10 mdui-col-offset-xs-1 mdui-typo"><h1>' + response.data.title + "</h1>"
+    html += converter.makeHtml(article)
+    html += '</div>'
     var div = document.getElementById('articles')
-    div.innerHTML=html
+    div.innerHTML = html
   })
-}// 读取文章(需要用到graphQL)
-// 筛选标签文章(需要用到graphQL)
+}// 读取文章
+// 筛选标签文章(需要用到 graphQL)
+listArticles();
