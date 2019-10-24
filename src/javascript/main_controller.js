a7dc237b06bd9db1b4d6c1225b13110d7b3a7253
swj <971210502@qq.com>
2019-10-24 23:10:05 +0800

    大更新,使用vue重构主页,下次更新将后完全使用vue

--- src/javascript/main_controller.js
+++ src/javascript/main_controller.js
@@ -1,4 +1,43 @@
-(function () {
+console.time(); // 咋瓦鲁多!
+
+var vue_component = JSON.parse(`{
+  "el":"#vue-controller",
+  "data":{
+    "name":"YaoZ",
+    "articleList":[{
+      "issueId":"怒",
+      "articles_title":"怒",
+      "article_date":"怒",
+      "label":[]
+    }],
+    "labelList":[{
+      "label_color":"怒",
+      "label_name":"怒"
+    }],
+    "commentList":[{
+      "comment_avartar_url":"怒",
+      "comment_user_name":"怒",
+      "comment_content":"怒"
+    }],
+    "articleContent":{
+      "article_title":"怒",
+      "article_content":"怒"
+    }
+  },
+  "method":{
+
+  }
+}`)
+
+function getVueJson(){
+  return function(){
+    return vue_component
+  }()
+} // 用闭包把封装的变量暴露给vue_controller.js
+// 目前没有暴露出去,所以vue无法渲染
+// 原先的实现方式也已删除,因此没有任何东西渲染出来
+
+(function () { // 标签列表
   axios.get('https://api.github.com/repos/11zi/11zi.github.io/labels').then(function (response) {
     var labels = response.data
     var labelElement = document.getElementById('labels')
@@ -7,8 +46,8 @@
       labelElement.innerHTML += append
     }
   })
-})();// 标签列表
-(function () {
+})();
+(function () { // 评论列表
   axios.get('https://api.github.com/repos/11zi/11zi.github.io/issues/1/comments').then(function (response) {
     var comments = response.data
     var commentElement = document.getElementById('comments')
@@ -20,8 +59,8 @@
       commentElement.innerHTML += append
     }
   })
-}());// 评论列表
-function listArticles() {
+}());
+function listArticles() { // 展示文章列表
   axios.get('https://api.github.com/repos/11zi/11zi.github.io/issues').then(function (response) {
     var articles = response.data
     var articleElement = document.getElementById('articles')
@@ -38,10 +77,9 @@ function listArticles() {
     }
     articleElement.innerHTML = htmlList
   })
-};// 展示文章列表
-function getArticle(issueID) {
+};
+function getArticle(issueID) { // 读取文章
   axios.get('https://api.github.com/repos/11zi/11zi.github.io/issues/' + issueID).then(function (response) {
-    console.log("Okay no pb.")
     var converter = new showdown.Converter()
     var article = response.data.body
     var html = '<div class="mdui-col-xs-10 mdui-col-offset-xs-1 mdui-typo"><h1>' + response.data.title + "</h1>"
@@ -50,6 +88,8 @@ function getArticle(issueID) {
     var div = document.getElementById('articles')
     div.innerHTML = html
   })
-}// 读取文章
-// 筛选标签文章(需要用到 graphQL)
-listArticles();
+}
+// 筛选标签文章(需要用到 graphQL V4的api 目前无法完成)
+
+console.timeEnd();
+// 时间开始流动
